2e096361f18e0e8c9816194f94730e1d2d207ac3
vehere-ccu7 <52567310+vehere-ccu7@users.noreply.github.com>
2021-01-07 18:28:25 +0530

    Update ssdeep.groovy

--- ssdeep.groovy
+++ ssdeep.groovy
@@ -10,33 +10,3 @@ for(int i=0; i<doc[field].size(); i++){
     }
 }
 return scoreMaxx;
- curl -X GET "localhost:9200/_search?pretty" -H 'Content-Type: application/json' -d'
-{
-  "query": {
-    "function_score": {
-      "query": {
-        "exists": {
-          "field": "recon.context_hash"
-        }
-      },
-      "min_score" : 0,
-        "functions": [
-          {
-            "script_score": {
-              "script": {
-                "file": "ssdeep",
-                "lang": "groovy",
-                 "params":{
-                   "field" : "recon.context_hash.keyword",
-                   "term" : "48:rj1f61/gMqYyxco7jNhmjMn1Op0jhUFp51DIUWn1YYU2:ff61/RIcqjWwn0pYUFpJNu"
-              }
-            }
-          }
-        }
-      ]
-    }
-  },
-  "_source": ["recon.context_hash"]
-}
-'
-
