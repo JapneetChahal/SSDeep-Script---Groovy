77d9199f8de7fe2244ca7496c64125c4adb65fee
vehere-ccu7 <52567310+vehere-ccu7@users.noreply.github.com>
2021-01-08 15:14:47 +0530

    Update ssdeep.groovy

--- ssdeep.groovy
+++ ssdeep.groovy
@@ -1,10 +1,10 @@
 import info.debatty.java.spamsum.*;
 def scorer = new SpamSum();
-int  scoreMaxx = 0;
+int scoreMaxx = 0;
 int scoreCurr = 0;
-
-for(int i=0; i<doc[field].size(); i++){
-    scoreCurr = scorer.match(doc[field][i], term);
+String fieldNew = field.endsWith(".keyword")?field:field+".keyword";
+for(int i=0; i<doc[fieldNew].size(); i++){
+    scoreCurr = scorer.match(doc[fieldNew][i], term);
     if(scoreCurr > scoreMaxx){
         scoreMaxx = scoreCurr;
     }
